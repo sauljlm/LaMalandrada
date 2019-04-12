e9a90932667117643da4f563a2860033f4c0a353
Saul Lopez Molina <saulsjlmclash@gmail.com>
2019-04-12 08:39:48 +0000

    version 1.3

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 const btnProyecto = document.querySelector('.proyectos');
 const btnMiembros = document.querySelector('.miembros');
-const contPost = document.querySelector('.cont-posts');
+const contPost = document.querySelector('.container-posts');
 const contAboutUs = document.querySelector('.cont-About-us');
 
-let hola = false;
+let state = false;
 btnMiembros.addEventListener('click', (e)=> {
   e.preventDefault();
   contAboutUs.classList.toggle('hide');
   contPost.classList.toggle('hide');
-  if (!hola) {
+  if (!state) {
     btnProyecto.innerHTML = 'blog';
-    hola = true;
+    state = true;
   } else {
     btnProyecto.innerHTML = 'PROYECTO';
-    hola = false;
+    state = false;
   }
 });
 btnProyecto.addEventListener('click', (e)=> {
   e.preventDefault();
   contAboutUs.classList.toggle('hide');
   contPost.classList.toggle('hide');
-  if (!hola) {
+  if (!state) {
     btnProyecto.innerHTML = 'blog';
-    hola = true;
+    state = true;
   } else {
     btnProyecto.innerHTML = 'PROYECTO';
-    hola = false;
+    state = false;
   }
 });
